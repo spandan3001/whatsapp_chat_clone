09a3321e1dfd25f8460abdb6d06d805b1eef4c01
spandanmn <tyian3001@gmail.com>
2024-02-29 02:12:28 +0530

    feat: added chat screen(chat_screen.dart)

--- lib/data/mock_data/dummy_data.dart
+++ lib/data/mock_data/dummy_data.dart
@@ -6,13 +6,13 @@ List<ChatModel> dummyData = [
       message: "Hey Flutter, You are so amazing !",
       time: "15:30",
       avatarUrl:
-      "http://www.usanetwork.com/sites/usanetwork/files/styles/629x720/public/suits_cast_harvey.jpg?itok=fpTOeeBb"),
+      "https://imgs.search.brave.com/qp19jzXKVPn-a-nTiJIU2wnkcOWcpG3Gwi0KnrVOJYU/rs:fit:500:0:0/g:ce/aHR0cHM6Ly90NC5m/dGNkbi5uZXQvanBn/LzAyLzg2LzQ3LzA5/LzM2MF9GXzI4NjQ3/MDk2NV92aWFITTZa/TmpyY3ZMRU5lMTR5/V1FGaTFqRW0yaktl/Vi5qcGc"),
   ChatModel(
       name: "Bob",
       message: "Hey I have hacked whatsapp!",
       time: "17:30",
       avatarUrl:
-      "http://www.usanetwork.com/sites/usanetwork/files/styles/629x720/public/suits_cast_harvey.jpg?itok=fpTOeeBb"),
+      "https://imgs.search.brave.com/VyZ-FerK3a-SRHg22VpNYJ_KQN_F1N0rRpfw2unMYPY/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vaWQvNDky/NTI5Mjg3L3Bob3Rv/L3BvcnRyYWl0LW9m/LWhhcHB5LWxhdWdo/aW5nLW1hbi5qcGc_/cz02MTJ4NjEyJnc9/MCZrPTIwJmM9MHhR/Y2Q2OUJmLW1Xb0pZ/Z2p4QlNQZzdGSFM1/N25PZllwWmFabFlE/VktSRT0"),
   ChatModel(
       name: "Charlie",
       message: "Wassup !",
@@ -30,7 +30,7 @@ List<ChatModel> dummyData = [
       message: "I'm the fastest man alive!",
       time: "12:30",
       avatarUrl:
-      "http://www.usanetwork.com/sites/usanetwork/files/styles/629x720/public/suits_cast_harvey.jpg?itok=fpTOeeBb"),
+      "https://imgs.search.brave.com/VyZ-FerK3a-SRHg22VpNYJ_KQN_F1N0rRpfw2unMYPY/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vaWQvNDky/NTI5Mjg3L3Bob3Rv/L3BvcnRyYWl0LW9m/LWhhcHB5LWxhdWdo/aW5nLW1hbi5qcGc_/cz02MTJ4NjEyJnc9/MCZrPTIwJmM9MHhR/Y2Q2OUJmLW1Xb0pZ/Z2p4QlNQZzdGSFM1/N25PZllwWmFabFlE/VktSRT0"),
   ChatModel(
       name: "Frank",
       message: "Hey Flutter, You are so cool !",
--- lib/views/chat_screen.dart
+++ lib/views/chat_screen.dart
@@ -1,14 +1,49 @@
 
 import 'package:flutter/material.dart';
 
+import '../data/mock_data/dummy_data.dart';
+
 class ChatScreen extends StatelessWidget {
   const ChatScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-        body:
-        Center(child: Text("CHAT SCREEN"))
+    return ListView.builder(
+      itemCount: dummyData.length,
+      itemBuilder: (context, i) => Column(
+        children: <Widget>[
+          const Divider(
+            height: 10.0,
+          ),
+          ListTile(
+            leading: CircleAvatar(
+              foregroundColor: Theme.of(context).primaryColor,
+              backgroundColor: Colors.grey,
+              backgroundImage: NetworkImage(dummyData[i].avatarUrl),
+            ),
+            title: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text(
+                  dummyData[i].name,
+                  style: const TextStyle(fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  dummyData[i].time,
+                  style: const TextStyle(color: Colors.grey, fontSize: 14.0),
+                ),
+              ],
+            ),
+            subtitle: Container(
+              padding: const EdgeInsets.only(top: 5.0),
+              child: Text(
+                dummyData[i].message,
+                style: const TextStyle(color: Colors.grey, fontSize: 15.0),
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
