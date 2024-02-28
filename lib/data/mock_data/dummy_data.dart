94139e8419a12c5970c3e7b2a34e200b0d533116
spandanmn <tyian3001@gmail.com>
2024-02-29 00:21:49 +0530

    feat: added chat model and mock data

---
+++ lib/data/mock_data/dummy_data.dart
@@ -0,0 +1,40 @@
+import '../models/char_model.dart';
+
+List<ChatModel> dummyData = [
+  ChatModel(
+      name: "Alice",
+      message: "Hey Flutter, You are so amazing !",
+      time: "15:30",
+      avatarUrl:
+      "http://www.usanetwork.com/sites/usanetwork/files/styles/629x720/public/suits_cast_harvey.jpg?itok=fpTOeeBb"),
+  ChatModel(
+      name: "Bob",
+      message: "Hey I have hacked whatsapp!",
+      time: "17:30",
+      avatarUrl:
+      "http://www.usanetwork.com/sites/usanetwork/files/styles/629x720/public/suits_cast_harvey.jpg?itok=fpTOeeBb"),
+  ChatModel(
+      name: "Charlie",
+      message: "Wassup !",
+      time: "5:00",
+      avatarUrl:
+      "http://www.usanetwork.com/sites/usanetwork/files/styles/629x720/public/suits_cast_harvey.jpg?itok=fpTOeeBb"),
+  ChatModel(
+      name: "David",
+      message: "I'm good!",
+      time: "10:30",
+      avatarUrl:
+      "http://www.usanetwork.com/sites/usanetwork/files/styles/629x720/public/suits_cast_harvey.jpg?itok=fpTOeeBb"),
+  ChatModel(
+      name: "Eve",
+      message: "I'm the fastest man alive!",
+      time: "12:30",
+      avatarUrl:
+      "http://www.usanetwork.com/sites/usanetwork/files/styles/629x720/public/suits_cast_harvey.jpg?itok=fpTOeeBb"),
+  ChatModel(
+      name: "Frank",
+      message: "Hey Flutter, You are so cool !",
+      time: "15:30",
+      avatarUrl:
+      "http://www.usanetwork.com/sites/usanetwork/files/styles/629x720/public/suits_cast_harvey.jpg?itok=fpTOeeBb"),
+];
