aac0a6648d277c6a78a4c4ed0c289838386368a7
spandanmn <tyian3001@gmail.com>
2024-03-01 17:19:22 +0530

    feat: added methods to coordinate the chat_detail_screen

--- lib/controllers/main_controller.dart
+++ lib/controllers/main_controller.dart
@@ -1,19 +1,57 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
+
+import '../data/mock_data/dummy_data.dart';
+import '../data/models/chat_model.dart';
+import '../data/models/user_model.dart';
+
 class MainController extends GetxController {
-  late TabController tabController; // Tab controller for managing tabs
-  Rx<bool> showFab = true.obs; // Observable boolean for showing the floating action button
+
+
+  // Text controller for the input message
+  late final TextEditingController messageController;
+
+  // Tab controller for managing tabs
+  late final TabController tabController;
+
+  // Observable boolean for showing the floating action button
+  final RxBool showFab = true.obs;
+
+  // List to store chat entries, each associated with a user
+  final RxList<MapEntry<User, List<Chat>>> chats = <MapEntry<User, List<Chat>>>[].obs;
+
+
 
   // Initialize the controller
   void init(dynamic state) {
-    tabController = TabController(initialIndex: 1, length: 4, vsync: state); // Initialize the tab controller
-    tabController.addListener(() { // Add a listener to the tab controller
-      if (tabController.index == 1) { // If the current tab index is 1 (Chats tab)
-        showFab(true); // Show the floating action button
-      } else {
-        showFab(false); // Otherwise, hide the floating action button
-      }
+    // Initialize the tab controller
+    tabController = TabController(initialIndex: 1, length: 4, vsync: state);
+
+    messageController = TextEditingController();
+    // Add a listener to the tab controller
+
+    tabController.addListener(() {
+      showFab(tabController.index == 1);
     });
+
+    // Load chat data when the controller is initialized
+    getChats();
+  }
+
+  // Method to load initial chat data
+  void getChats() {
+    // Clear existing chat entries
+    chats.clear();
+    // Iterate through users and add chat entries
+    for (var user in users) {
+      chats.add(MapEntry(user, [...dummyData]));
+    }
+  }
+
+  // Method to sort chat messages by time in descending order
+  void sortChatsByTimeDescending(int chatIndex) {
+    chats[chatIndex].value.sort((a, b) => b.time.compareTo(a.time));
   }
 }
